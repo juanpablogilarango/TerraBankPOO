e3eb34ab92f66fa475bc4713b2b58fe0c92047ec
CarlosFranco97 <carlosfranco63.cf@gmail.com>
2024-12-12 15:44:13 -0500

    Se agrega el atributo de tipo de cliente a customer

--- src/main/java/terrabank/users/Customer.java
+++ src/main/java/terrabank/users/Customer.java
@@ -1,51 +1,14 @@
 package terrabank.users;
 
 public class Customer extends Users{
-    private int  numberCreditCard, creditCardQuota;
-    private String typeCard;
-    private double creditAmount;
+    String typeCustomer;
 
     //Constructor of customer:
     public Customer(String email, String nameUser, String password){
         super(email, nameUser ,password);
     }
 
-    //Methods:
 
-
-
-    //getters and setters:
-    public void setTypeCard(String typeClient){
-        this.typeCard = typeClient;
-    }
-
-    public String getTypeCard(){
-        return this.typeCard;
-    }
-
-    public void setNumberCreditCard(int numberCreditCard){
-        this.numberCreditCard = numberCreditCard;
-    }
-
-    public int getNumberCreditCard(){
-        return numberCreditCard;
-    }
-
-    public void setCreditCardQuota(int creditCardQuota){
-        this.creditCardQuota = creditCardQuota;
-    }
-
-    public int getCreditCardQuota(){
-        return creditCardQuota;
-    }
-
-    public void setCreditAmount(double creditAmount) {
-        this.creditAmount = creditAmount;
-    }
-
-    public double getCreditAmount(){
-        return creditAmount;
-    }
 
 
 }
